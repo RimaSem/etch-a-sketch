aef54c73f8f43044fe70ed3419e2ad2067f92181
RimaSem <rima.semetaite@gmail.com>
2022-11-21 09:18:16 +0200

    Add erase, black and rainbow functions

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const grid = document.querySelector(".grid");
 createGrid(16);
 
 const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-  cell.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "black";
-  });
-});
+selectBlack();
 
 const clearBtn = document.querySelector(".clear-btn");
 clearBtn.addEventListener("click", clearGrid);
+
+const eraseBtn = document.querySelector(".erase-btn");
+eraseBtn.addEventListener("click", erase);
+
+const blackBtn = document.querySelector(".black-btn");
+blackBtn.addEventListener("click", selectBlack);
+
+const rainbowBtn = document.querySelector(".rainbow-btn");
+rainbowBtn.addEventListener("click", selectRainbow);
 
 function createGrid(size) {
   for (let i = 0; i < size; i++) {
@@ -28,4 +33,32 @@ function createGrid(size) {
 
 function clearGrid() {
   cells.forEach((cell) => (cell.style.backgroundColor = "white"));
+  selectBlack();
+}
+
+function erase() {
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "white";
+    });
+  });
+}
+
+function selectBlack() {
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+  });
+}
+
+function selectRainbow() {
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseover", (e) => {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    });
+  });
 }
